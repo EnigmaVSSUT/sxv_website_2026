a0f2cb11dd3f1d6707ace02821feda382caf267e
shaswatHota <shaswathota04@gmail.com>
2025-12-31 20:32:45 +0530

    add : events fetching from db

--- utils/api.ts
+++ utils/api.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sxv-backend.vercel.app/",
+  baseURL: "https://sxv-backend-eight.vercel.app/",
   headers: {
     "Content-Type": "application/json",
-    "Accept":"application/json"
+    "Accept": "application/json"
   },
+  withCredentials: true
 });
 
 export default api;
